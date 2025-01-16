c2f380615ebbe74d3f75e6c1f6d7eddd51a0d4f2
Ahmad Riza Muzakki <ahmadmuzakki60@gmail.com>
2025-01-16 22:48:04 +0700

    penambahan tampilan utama admin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./view/**.{html,js}"],
+  content: ["./**.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
